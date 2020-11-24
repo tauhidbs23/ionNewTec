510e2a90307fbfd12513df98dbab983d6a1ae652
tauhidbs23 <tauhidul.islam@brainstation23.com>
2020-11-24 14:49:47 +0600

    phone number validation done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  userAddressValidations: FormGroup;
+  
+  constructor(private fb: FormBuilder) { }
+  ngOnInit() {
+    this.userAddressValidations = this.fb.group({
+      mobile: ['', Validators.compose([Validators.pattern(
+      '(([+][(]?[0-9]{1,3}[)]?)|([(]?[0-9]{4}[)]?))\s*[)]?[-\s\.]?[(]?[0-9]{1,3}[)]?([-\s\.]?[0-9]{3})([-\s\.]?[0-9]{3,4})'
+        )])]
+    });
+
+  }
 
 }
