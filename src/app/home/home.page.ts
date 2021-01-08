3be254f6d3d154cd717d5f8b89ea96e3ad60e3e1
tauhidbs23 <tauhidul.islam@brainstation23.com>
2021-01-08 12:59:28 +0600

    camera added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CameraPreview } from '@ionic-native/camera-preview/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  base64:string='data:image/png;base64,';
 
-  constructor() {}
+  constructor(private preview:CameraPreview) {}
+
+  ngOnInit(){
+    this.openCamere();
+  }
+
+  openCamere(){
+    this.preview.startCamera({}).then((val)=>{
+      alert(val);
+      this.preview.takePicture().then((base65)=>{
+        console.log('only base 64', base65);
+        this.base64 = this.base64 + base65;
+        console.log('base url->', this.base64);
+        this.preview.stopCamera();
+      })
+
+    },(err)=>{
+      alert(JSON.stringify(err));
+
+    });
+  }
 
 }
